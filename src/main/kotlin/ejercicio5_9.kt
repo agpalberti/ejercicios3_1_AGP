78c05e8ecac2ad9ec5c1f41f1f9603086e21a1fe
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-10-22 19:51:15 +0200

    5.9 terminao

--- src/main/kotlin/ejercicio5_9.kt
+++ src/main/kotlin/ejercicio5_9.kt
@@ -1,21 +1,75 @@
 //Ejercicio 5.9
 class Cuenta(val numerocuenta:String, var saldo:Double = .0){
 
-    fun pago(numerocuenta: String,cantidad:Double){
+    fun pago(cantidad:Double){
         saldo -= cantidad
+    }
+
+    fun abono(cantidad: Double){
+        saldo += cantidad
+    }
+
+    fun transfer(cantidad:Double, cuentadestino:Cuenta){
+        cuentadestino.abono(cantidad)
+        this.saldo -= cantidad
 
     }
 
+    override fun toString(): String {
+        return "[${this.numerocuenta}]"
+    }
+}
+
+class Persona(val DNI:String, val cuentas:MutableList<Cuenta> = mutableListOf()){
+
+    init {
+        require(cuentas.size<=3){"No puedes añadir más de 3 cuentas"}
+    }
+
+    fun addcuenta(nuevacuenta:Cuenta){
+        this.cuentas.add(nuevacuenta)
+    }
+
+    fun esmorosa():String{
+        for(i in 0..2){
+            if (cuentas[i].saldo<0) return "Es moroso"
+        }
+        return "No es moroso"
+    }
+
+    override fun toString(): String {
+        return this.DNI
+    }
 
 }
 
-class Persona(){
-
+fun saldos(cuenta:Cuenta){
+    println("Saldo de la cuenta $cuenta: ${cuenta.saldo}")
 }
 
 
 fun main(){
+    val persona1 = Persona("ASDFSADF")
+    val cuenta1 = Cuenta("4774 7887 2929 9010")
+    val cuenta2 = Cuenta("4851 9461 9471 5794",700.00)
+    persona1.addcuenta(cuenta1)
+    persona1.addcuenta(cuenta2)
 
+    saldos(cuenta1)
+    saldos(cuenta2)
+
+    println("Abono de 1100€ a la cuenta $cuenta1")
+    cuenta1.abono(1100.00)
+    println("Pago de 750€ a la cuenta $cuenta2")
+    cuenta2.pago(750.00)
+    saldos(cuenta1)
+    saldos(cuenta2)
+    println(persona1.esmorosa())
+
+    println("Transferencia de 500€ de la cuenta $cuenta1 a la cuenta $cuenta2")
+    cuenta1.transfer(500.00,cuenta2)
+    saldos(cuenta1)
+    saldos(cuenta2)
 }
 
 
