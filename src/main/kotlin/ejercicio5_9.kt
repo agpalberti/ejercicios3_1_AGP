127cf27a20833e72e131d77d3b78432882704e5a
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-10-22 18:29:51 +0200

    primer try

---
+++ src/main/kotlin/ejercicio5_9.kt
@@ -0,0 +1,21 @@
+//Ejercicio 5.9
+class Cuenta(val numerocuenta:String, var saldo:Double = .0){
+
+    fun pago(numerocuenta: String,cantidad:Double){
+        saldo -= cantidad
+
+    }
+
+
+}
+
+class Persona(){
+
+}
+
+
+fun main(){
+
+}
+
+
