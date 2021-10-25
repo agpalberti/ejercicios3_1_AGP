1cc709191f931887f9dfe39b527c223869513b6e
agpalberti <agonzalezp@iesrafaelalberti.es>
2021-10-25 21:40:11 +0200

    terminar ConjuntoLibros

--- src/main/kotlin/ejercicio5_10.kt
+++ src/main/kotlin/ejercicio5_10.kt
@@ -1,14 +1,27 @@
 //Ejercicio 5.10
 
-class Libro(val titulo: String, val autor: String, val numpag: Int, var calif: Byte) {
+class Libro(val titulo: String, val autor: String, val numpag: Int, calificacion: Byte) {
+    var calif: Byte = calificacion
+        set(value) {
+            if (value in 0..10) field = value
+            else throw IndexOutOfBoundsException("La calificación debe estar entre 0 y 10")
+        }
+
     init {
-        require(calif in 0..10){"La calificación debe estar entre 0 y 10"}
+        require(calif in 0..10) { "La calificación debe estar entre 0 y 10" }
     }
 
 }
 
-class ConjuntoLibros(){
-    var libros:Array<Libro> = arrayOf()
+class ConjuntoLibros() {
+    var libros: Array<Libro?> = arrayOfNulls<Libro>(10)
+
+    fun add(libro: Libro) {
+        for (i in 0..10){
+
+        }
+    }
+
 }
 
 
